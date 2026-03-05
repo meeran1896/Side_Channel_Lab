3c212f932f79de61bd37b09684d34347940bb6a2
Mohamed Meeran <80145573+meeran1896@users.noreply.github.com>
2026-03-05 02:44:34 -0500

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+import random
+
+app = Flask(__name__)
+SECRET = "alamak"
+
+@app.route("/check")
+def check():
+    guess = request.args.get("q", "")
+    if SECRET.startswith(guess):
+        padding = "A" * 1000
+    else:
+        padding = "A" * 500
+    noise = "C" * random.randint(0, 100)
+    return padding + noise
+
+app.run(host="0.0.0.0", port=5001)
